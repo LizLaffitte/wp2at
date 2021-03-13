ac4e689d230d5dfd33712f65152b844b85195a71
lizlaffitte <laffitte.digital@gmail.com>
2021-03-13 14:05:47 -0500

    Initial commit.

---
+++ lib/wp2at.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wp2at/version"
+
+module Wp2at
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wp2at/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Wp2at
+  VERSION = "0.1.0"
+end
