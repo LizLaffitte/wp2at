6863f5b779f3015460a4a4cfc4923a04d430f982
lizlaffitte <laffitte.digital@gmail.com>
2021-03-15 08:09:17 -0400

    Save username when added; add blog option.

--- lib/wp2at/option.rb
+++ lib/wp2at/option.rb
@@ -5,9 +5,12 @@ class Wp2at::Option
     def execute(args)
         command = args[0]
         options = args[1]
+        @current_settings = Settings.exists? ? Settings.load : Settings.new
         case command
         when "userconfig"
-           add_username(options)
+            options ? add_username(options) : @current_settings.username
+        when "blog"
+            options ? add_blog(options) : @current_settings.list_blogs 
         else
             puts "That's not an option"
         end
@@ -15,7 +18,8 @@ class Wp2at::Option
     end
 
     def add_username(username)
-        @current_settings = Settings.new(username)
+        @current_settings.username = username
+        @current_settings.settings_save
         puts @current_settings.username
     end
 
