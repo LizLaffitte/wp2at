f312b9ee7369943bb2f2c0d0623cd0b403fe163e
lizlaffitte <laffitte.digital@gmail.com>
2021-03-14 21:31:25 -0400

    Start menu method.

--- lib/wp2at/cli.rb
+++ lib/wp2at/cli.rb
@@ -1,22 +1,40 @@
 require 'yaml'
 class CLI
+    options = {}
 
     def call
-      Settings.load ? welcome : new_user
+      Settings.load ? menu : new_user
+
     end
 
-    def welcome
-        puts "Hello!"
+
+    def menu
+        option = ""
+        puts "Welcome, #{@current_settings.username}!"
+        puts "Choose an option:"
+        
+        
+    end
+    
+    
+
+    def update
+        print_blogs
+    end
+
+    def print_blogs
+        @current_settings.blogs.sort_by(&:name).each.with_index(1){|blog, idx| puts "#{idx}. #{blog.name}" }
     end
 
     def new_user
         puts "Welcome, new user!"
         add_username
-        puts "Let's add a blog post"
-        add_blogs
-        puts "Great! You now have #{@current_settings.blog_count} blogs."
         puts "Now we need an AirTable API key."
         add_atapi
+        puts "Let's add a blog"
+        add_blogs
+        puts "Great! You now have #{@current_settings.blog_count} blogs."
+        menu
     end
 
     def edit_username
@@ -45,7 +63,15 @@ class CLI
             puts "Please add a URL for #{blog_name}."
             blog_url = gets.chomp   
             puts "#{blog_name}: #{blog_url}"
-            @current_settings.blogs << {blog_name: blog_url}
+            
+            puts "Enter the name of the base we'll be adding this blog info to."
+            base_name = gets.chomp
+            puts "Enter the id of the base."
+            base_id = gets.chomp
+            puts "Enter the table name."
+            table_name = gets.chomp
+            blog = Blog.new(blog_name,blog_url,base_name, base_id, table_name)
+            @current_settings.add_blog(blog)
             puts "Would you like to add a blog? (y/n)"
             add = gets.chomp.downcase
         end
