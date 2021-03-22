e13ae18b385e563a9910eae0e6069f5802c80100
lizlaffitte <laffitte.digital@gmail.com>
2021-03-21 20:59:15 -0400

    Stub out initizer method for Blog class. Ensure we're adding all the info at the same time.

--- lib/wp2at/blog.rb
+++ lib/wp2at/blog.rb
@@ -1,15 +1,25 @@
 class Blog
     attr_accessor :name, :url, :base_name, :base_id, :table
 
-    def initialize(name, url, base_name, base_id, table)
+    def initialize(name, url="", base_name="", base_id="", table="")
+        puts "Add a website for #{name}:"
+        url = STDIN.gets.chomp
+        puts "Add a base for #{name}"
+        base_name = STDIN.gets.chomp
+        puts "Add a base_id"
+        base_id = STDIN.gets.chomp
+        puts "Add a table name"
+        table = STDIN.gets.chomp
        @name = name
        @url = url
        @base_name = base_name
-       @base_id = base_id,
+       @base_id = base_id
        @table = table
     end
 
-
+    def missing_pieces(name)
+       
+    end
     
 
 end
