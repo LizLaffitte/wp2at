799b89250199e73fafe5e5dace540128cc0a70e9
lizlaffitte <laffitte.digital@gmail.com>
2021-03-21 21:00:52 -0400

    Add ability to add username correctly, and save that info into YAML.

--- lib/wp2at/option.rb
+++ lib/wp2at/option.rb
@@ -8,19 +8,20 @@ class Wp2at::Option
         @current_settings = Settings.exists? ? Settings.load : Settings.new
         case command
         when "userconfig"
-            options ? add_username(options) : @current_settings.username
-        when "blog"
-            options ? add_blog(options) : @current_settings.list_blogs 
+            puts options ? add_username(options) : @current_settings.username
+        when "blog" 
+            options ? @current_settings.add_blog(options) : @current_settings.list_blogs 
         else
             puts "That's not an option"
         end
     
     end
 
-    def add_username(username)
+
+    def add_username(username="")
         @current_settings.username = username
         @current_settings.settings_save
-        puts @current_settings.username
+        @current_settings.username
     end
 
 end
