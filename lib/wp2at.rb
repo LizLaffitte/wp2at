804eb33c2ca5441451cd86d9d4fbaaf01c97e532
lizlaffitte <laffitte.digital@gmail.com>
2021-03-21 21:12:32 -0400

    Require new API class so we can use it throughout the gem.

--- lib/wp2at.rb
+++ lib/wp2at.rb
@@ -5,6 +5,7 @@ require_relative "wp2at/cli"
 require_relative "wp2at/settings"
 require_relative "wp2at/blog"
 require_relative "wp2at/option"
+require_relative "wp2at/api"
 
 module Wp2at
   class Error < StandardError; end
