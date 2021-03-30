00fc384cfe7e5ded8fb44683f7bebf019949170b
lizlaffitte <laffitte.digital@gmail.com>
2021-03-29 21:53:19 -0400

    Add option for flags so we can prevent extra records from being added.

--- lib/wp2at/option.rb
+++ lib/wp2at/option.rb
@@ -5,6 +5,7 @@ class Wp2at::Option
     def execute(args)
         command = args[0]
         options = args[1]
+        flags = args[2]
         @current_settings = Settings.exists? ? Settings.load : Settings.new
         case command
         when "userconfig"
@@ -19,14 +20,14 @@ class Wp2at::Option
                     puts "Add a blog by running blog with an argument of the blog name you'd like to add."
                 end
                 if @current_settings.at_api != ""
-                    api = API.new(@current_settings)
-                    api.ping(options)
+                    blog = @current_settings.blogs.find{|blog| blog.name == options}
+                    api = API.new(@current_settings, blog)
+                    api.ping(flags)
                 else
                     puts "Add an AirTable API Key by running the command api-key and passing an API key."
                 end
             else
-                api = API.new(@current_settings)
-                api.list_blogs
+                puts "Add a blog to be synced"
             end
         else
             puts "That's not an option"
