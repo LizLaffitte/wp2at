3af85f1f70cd43afb4218da66ee9af73d95d0d51
lizlaffitte <laffitte.digital@gmail.com>
2021-04-17 08:59:19 -0400

    Abstracting comparison of data.

--- lib/wp2at/api.rb
+++ lib/wp2at/api.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 require 'pry'
 class API
-    attr_accessor :routes, :at_latest, :wp_latest, :total
+    attr_accessor :routes, :at_latest, :total
 
     @@wp_api = "/wp-json/wp/v2/posts"
     @@at_api = "https://api.airtable.com/v0/"
@@ -38,7 +38,7 @@ class API
         x = 1
         wp_posts = []
         until x > self.total
-            resp = HTTParty.get(@@wp_api + "?_fields=id,title,date,link" + "&per_page=100&page=" + x.to_s)
+            resp = HTTParty.get(@@wp_api + "?_fields=id,title,date,link,modified" + "&per_page=100&page=" + x.to_s)
             wp_posts.push(resp.parsed_response)
             total = resp.headers["x-wp-totalpages"].to_i
             x += 1
@@ -61,9 +61,11 @@ class API
             post.delete("title")
             post[@current_settings.headers[:date]] = post.delete("date")
             post[@current_settings.headers[:url]] = post.delete("link")
+            post.delete("modified")
             records.push({:fields => post})
         end
         records
+
     end
 
     def ping_wp
@@ -72,7 +74,6 @@ class API
         rescue
             false
         else
-            @wp_latest = resp.parsed_response[0]["id"]
             @total = resp.headers["x-wp-totalpages"].to_i
             true
         end
@@ -80,13 +81,13 @@ class API
     def sync(flags)
         if ping_wp
             post_data = collect_post_data()
-            rows = collect_row_data
-            all_data = compare_datasets(rows.keys, post_data[:ids])
-            if all_data[:new]
-                data = prep_data(post_data[:posts].keep_if{|post| new_posts.include? post["id"]})
+            at_data = collect_row_data
+            all_data = compare_datasets(at_data, post_data)
+            if all_data[:new].count > 0
+                data = prep_data(post_data[:posts].keep_if{|post| all_data[:new].include? post["id"]})
                 add_to_at(data, @@at_api)  
             else
-                binding.pry
+      
                 puts "All data up-to-date"
             end
         else
@@ -95,15 +96,18 @@ class API
         
     end
 
-    def compare_datasets(at_arr, wp_arr)
-        new_posts = wp_arr - at_arr
-        post_data = {:current => at_arr}
-        if new_posts.count > 0 
-            post_data[:new] = new_posts
-        end
-        post_data
+    def compare_datasets(at_data, wp_data)
+        # to_update = find_updates(wp_data, at_data)
+        all_posts = {
+            :new=> at_data.keys - wp_data[:ids],
+            # :update=> to_update
+        }
     end
 
+    def find_updates(wp_data, at_data)
+        
+        binding.pry
+    end
 
     def add_to_at(data, at_route)
         data.each_slice(10) do |slice| 
