d5b43ab437c7a9cc581d07f5210fb5cfa4402025
lizlaffitte <laffitte.digital@gmail.com>
2021-04-22 21:25:39 -0400

    Add method to URL encode params.

--- lib/wp2at/api.rb
+++ lib/wp2at/api.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'pry'
 class API
     attr_accessor :routes, :at_latest, :total
 
@@ -6,7 +7,7 @@ class API
     @@at_api = "https://api.airtable.com/v0/"
 
     def initialize(settings, blog, flags="")
-        @current_settings = settings
+        @cs = settings
         @blog = blog
         @@wp_api.prepend(blog.url)
         @@at_api += @blog.base_id + "/" + replace_space(@blog.table) 
@@ -16,9 +17,10 @@ class API
     def collect_row_data(post_hash)
         offset = ""
         loop do
-            at_response = call_at("fields%5B%5D=#{@current_settings.headers[:id]}",offset)
+            at_response = call_at(code(["fields[]", @cs.headers[:id]], ["filterByFormula", "NOT({Post ID} = '')"]),offset)
+            binding.pry
             at_response.parsed_response["records"].collect do |post| 
-                post_hash[post["fields"][@current_settings.headers[:id]]]["at"] =  post["id"]
+                post_hash[post["fields"][@cs.headers[:id]]]["at"] =  post["id"]
             end
             offset = at_response.parsed_response["offset"]
             break if !at_response.parsed_response["offset"]
@@ -29,7 +31,7 @@ class API
     def call_at(params ="", offset="")
         HTTParty.get(@@at_api + "?" + params + "&offset=" + offset, 
             :headers => {
-                "Authorization" => "Bearer #{@current_settings.at_api}", 
+                "Authorization" => "Bearer #{@cs.at_api}", 
                 "Content-Type" => "application/json"
             })
     end
@@ -52,18 +54,23 @@ class API
         post_data
     end
     
+    def code(*args)
+        subs = {"%28" => "(", "%29" => ")", "%27" => "'"}
+        return URI.encode_www_form(args).gsub(/(%28|%27|%29)/,subs)
+    end
+
     def replace_space(string)
-        return string.gsub(" ","%20")
+        return string.gsub(" ", "%20")
     end
 
     def prep_data(results, updates=false)
         records = []
         results.collect do |id, post|
-            post[@current_settings.headers[:id]] = post.delete("id")
-            post[@current_settings.headers[:title]] = post["title"].delete("rendered")
+            post[@cs.headers[:id]] = post.delete("id")
+            post[@cs.headers[:title]] = post["title"].delete("rendered")
             post.delete("title")
-            post[@current_settings.headers[:date]] = post.delete("date")
-            post[@current_settings.headers[:url]] = post.delete("link")
+            post[@cs.headers[:date]] = post.delete("date").split("T").first
+            post[@cs.headers[:url]] = post.delete("link")
             if updates
                 id = post["at"]
                 post.delete("at")
@@ -121,7 +128,7 @@ class API
         data.each_slice(10) do |slice| 
             at_response = HTTParty.post(at_route, 
             :body => {:records => slice}.to_json,
-            :headers => {"Authorization" => "Bearer #{@current_settings.at_api}", "Content-Type" => "application/json"}
+            :headers => {"Authorization" => "Bearer #{@cs.at_api}", "Content-Type" => "application/json"}
              )
             if at_response["error"]
                 case at_response["error"]["type"]
@@ -129,7 +136,11 @@ class API
                     puts "Incorrect table header(s). Try renaming #{at_response["error"]["message"].split("name: ")[1]}"
                     break
                 else
-                    puts "AirTable Error:" +  at_response["error"]
+                    if at_response["error"]["message"]
+                        puts "AirTable Error:" +  at_response["error"]["message"]
+                    else 
+                        puts "AirTable Error:" +  at_response["error"]
+                    end
                     break
                 end
             end
@@ -141,7 +152,7 @@ class API
         data.each_slice(10) do |slice| 
             at_response = HTTParty.patch(at_route, 
             :body => {:records => slice}.to_json,
-            :headers => {"Authorization" => "Bearer #{@current_settings.at_api}", "Content-Type" => "application/json"}
+            :headers => {"Authorization" => "Bearer #{@cs.at_api}", "Content-Type" => "application/json"}
              )
             if at_response["error"]
                 case at_response["error"]["type"]
